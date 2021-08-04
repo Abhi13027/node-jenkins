82acc91ce311dd7488553dcdce22161bf2c9c079
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-08-04 23:06:41 +0530

    Changes in the build script

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.get("/abhishek", (req, res) => {
 
 app.get("/shubham", (req, res) => {
   res.send("Shubham");
-})
+});
 
 app.get("/gaurav", (req, res) => {
   res.send("Gaurav");
-})
+});
 app.listen(5000, () => {
   console.log("The server is up and running");
 });
--- test/test.js
+++ test/test.js
@@ -7,3 +7,10 @@ describe("GET /", function () {
     request(app).get("/").expect("Hello Abhishek", done);
   });
 });
+
+describe("GET /abhishek", function () {
+  it("respond with Hello Abhishek", function (done) {
+    //navigate to root and check the response is "hello world"
+    request(app).get("/abhishek").expect("Abhishek", done);
+  });
+});
