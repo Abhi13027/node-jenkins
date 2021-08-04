bf59900ae23b7e09dee27cbb292a55aa1561b5eb
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-08-04 20:47:44 +0530

    exp 2

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require("supertest");
 var app = require("../index.js");
 
 describe("GET /", function () {
-  it("respond with Hello World", function (done) {
+  it("respond with Hello Abhishek", function (done) {
     //navigate to root and check the response is "hello world"
     request(app).get("/").expect("Hello Abhishek", done);
   });
