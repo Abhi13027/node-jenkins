8e5bbab041431ce1030daabdd4668aaca555b416
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-08-04 21:31:58 +0530

    New file 2

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/abhishek", (req, res) => {
   res.send("Abhishek");
 });
 
+app.get("/shubham", (req, res) => {
+  res.send("Shubham");
+})
+
 app.listen(5000, () => {
   console.log("The server is up and running");
 });
