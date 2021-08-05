91df448469ff8f61e0ddc998ab91c0b958462cb8
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-08-05 08:52:22 +0530

    Added failing and passing test

---
+++ test/temp.js
@@ -0,0 +1,16 @@
+var request = require("supertest");
+var app = require("../index.js");
+
+describe("GET /", function () {
+  it("respond with Hello Abhishek", function (done) {
+    //navigate to root and check the response is "hello world"
+    request(app).get("/").expect("Hello", done);
+  });
+});
+
+describe("GET /abhishek", function () {
+  it("respond with Hello Abhishek", function (done) {
+    //navigate to root and check the response is "hello world"
+    request(app).get("/abhishek").expect("Abhishek", done);
+  });
+});
